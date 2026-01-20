f7b861d7d6ce6da30aa6ea82b8c782c9afd679aa
pareilly27 <145300226+pareilly27@users.noreply.github.com>
2026-01-20 13:08:33 -0600

    Work Page Updates
    Resize, compressed images
    Adjusted Grid

--- grid/toggle.js
+++ grid/toggle.js
@@ -3,6 +3,9 @@ const toggleCircle = document.getElementById('toggleCircle')
 const toggleLabel = document.getElementById('toggleLabel');
 const grid = document.getElementById('grid');
 const bubbleCanvas = document.getElementById('bubbleCanvas');
+const projectGrid = document.getElementById('grid-container');
+const selectedText = document.getElementById('selected-text-container');
+const contactSection = document.getElementById('contact-section')
 
 toggleCircle.addEventListener('click', () => {
     grid.classList.toggle('active-view');
@@ -11,7 +14,13 @@ toggleCircle.addEventListener('click', () => {
     
     if (toggleBtn.classList.contains('toggled')) {
         toggleLabel.textContent = "Playtime";
+        selectedText.style.display = 'none';
+        projectGrid.style.display = 'none';
+        contactSection.style.display = 'none';
     } else {
         toggleLabel.textContent = "Precision";
+        selectedText.style.display = 'flex';
+        projectGrid.style.display = 'grid';
+        contactSection.style.display = 'block';
     }
 });
